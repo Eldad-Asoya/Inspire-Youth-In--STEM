20abebc10c7ff2f156957c26f7a06be58a04e095
Eldad <ypriest76@gmail.com>
2023-02-17 16:10:06 +0300

    Friday lessons on strings

---
+++ Week-2/bank.py
@@ -0,0 +1,31 @@
+#write a program that calculates 16% tax
+#ranging between 100k - 150k
+
+# 19% tax income is between 150k - 300k
+
+# 30% tax income if income is above 300k
+
+# 5% if income is less than 100k
+
+# print gross income and net income
+income = input("Enter your net income")
+
+
+
+poem = """this is a poem about nothing
+|   |   |  its funny how people laugh about nothing
+
+
+f_name="Eldad"
+s_name="Asoya"
+
+full_name = f_name + s_name
+
+
+age = "25"
+print("my name is"+full_name +str(age)+"years old")
+
+
+
+
+print("my name is {} and I am {}years) old ".format(full_name,age))
---
+++ Week-2/conditions.py
@@ -0,0 +1,17 @@
+age = 24
+gender = "male"
+
+if (age < 18):
+    print("You are still young")
+else:
+    print("You should get an id")
+
+
+
+
+#compound / multiple conditions
+
+if ((age > 30) $ (gender == "male")):
+    print("You qualify for a loan")
+else:
+    print("No loan for you")
