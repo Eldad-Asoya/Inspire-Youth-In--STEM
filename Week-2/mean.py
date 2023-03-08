f44fe56d5adc520b67b735fc914aecfc978a8e11
Eldad <125248775+Eldad-Asoya@users.noreply.github.com>
2023-03-08 16:10:37 +0300

    Delete Week-2 directory

--- Week-2/mean.py
+++
@@ -1,8 +0,0 @@
-
-
-numbers = [1,2,3,4,5,6,7,8,9]
-sum_num = 0
-for number in numbers:
-    sum_num = sum_num + number
-avg_num = sum_num / 10
-print("The avarage of the numbers :",avg_num)
--- Week-2/numbers.py
+++
@@ -1,7 +0,0 @@
-numbers =[1,2,3,4,5,6,7,8,9,10]
-
-for number in numbers:
-    number = int(input("Enter the number:"))
-    sum_num = sum_num + number
-
-    avg_num = sum_num / 10
